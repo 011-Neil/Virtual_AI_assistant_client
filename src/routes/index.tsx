a97b0c3bf8bfd76ee47e8e171ff211e757c78f77
unknown <neelpatel2412@gmail.com>
2026-05-07 23:54:46 +0530

    Fix React undefined in production

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useRef, useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import ReactMarkdown from "react-markdown";
