a3dcb144f4c56ae19791e8252933a3bbc5d29d91
unknown <neelpatel2412@gmail.com>
2026-05-07 23:43:00 +0530

    Fix Tailwind source path

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,12 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         tanstackStart(),
         netlify(),
+        tailwindcss()
     ],
 })
